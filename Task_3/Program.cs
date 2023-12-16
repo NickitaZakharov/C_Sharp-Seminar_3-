6a8956ff076c690a37a2766c688ee67fbbb45c69
Nickita <nickita.zaharoff2018@yandex.ru>
2023-12-16 12:37:03 +0300

    Добавил решение домашней работы

---
+++ Task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+Random random = new Random();
+double[] array = new double[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.NextDouble() + random.Next(1, 10);
+}
+double max = array[0];
+double min = array[0];
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+double difference = max - min;
+Console.WriteLine($"Разница между максимальным и минимальным элементами: {difference}");
---
+++ Task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿//Дано натуральное число в диапазоне от 1 до 100 000. 
+//Создайте массив, состоящий из цифр этого числа. Старший разряд числа должен располагаться на 0-м индексе массива, 
+//младший – на последнем. Размер массива должен быть равен количеству цифр.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int tempNumber = number;
+int count = 0;
+while (tempNumber > 0)
+{
+    count++;
+    tempNumber /= 10;
+}
+int[] array = new int[count];
+tempNumber = number;
+for (int i = count - 1; i >= 0; i--)
+{
+    array[i] = tempNumber % 10;
+    tempNumber /= 10;
+}
+foreach (int arr in array)
+{
+    Console.Write($"{arr},");
+}
